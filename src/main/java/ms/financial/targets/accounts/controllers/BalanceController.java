daecfefdffcb52d89be6dbd173dfb524fe208d49
Vinícius Rocha <vrocha.ciet@alelo.com.br>
2024-04-24 09:27:18 -0300

    corss adjustment on balance controller

--- src/main/java/ms/financial/targets/accounts/controllers/BalanceController.java
+++ src/main/java/ms/financial/targets/accounts/controllers/BalanceController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = "http://localhost:8080")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/balance")
